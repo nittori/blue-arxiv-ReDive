09ea678282f209fc5f6a22e5a34bfb0769a36a7d
nittori <gosick01ss@gmail.com>
2022-06-28 22:13:44 +0900

    final

--- createCSV.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-def writecsv():#csvファイルをSAVE
-    with open("csv.csv","a",newline='') as f: 
-        writer = csv.writer(f) 
-        writer.writerow(["test2"])
-    f.close()
-
-writecsv()
