17dbfdf9f04252c9e3400ad766b35148a4f71d97
nittori <gosick01ss@gmail.com>
2022-06-28 20:32:46 +0900

    workflow only

--- createCSV.py
+++ createCSV.py
@@ -3,7 +3,7 @@ import csv
 def writecsv():#csvファイルをSAVE
     with open("csv.csv","a",newline='') as f: 
         writer = csv.writer(f) 
-        writer.writerow(["test"])
+        writer.writerow(["test2"])
     f.close()
 
 writecsv()
