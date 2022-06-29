1f842a16f69b98b3e2e80e0da61e049eea6a29dc
nittori <gosick01ss@gmail.com>
2022-06-29 12:32:34 +0900

    heroku database

--- get_paper.py
+++ get_paper.py
@@ -4,6 +4,7 @@ import arxiv
 import yaml
 import os
 import csv
+import psycopg2
 
 def au_search(author):
   search = arxiv.Search(query ='au:"'+author+'"',max_results = 1,sort_by = arxiv.SortCriterion.SubmittedDate)
@@ -17,34 +18,34 @@ def read_config(file_dir):
         
     return authors["authors"]
 
-def read_db(file_dir):
-    db_path = f'{file_dir}/data/paper_database.csv'
-    
-    post_ids = []
-    
-    with open(db_path, 'r') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            post_ids.append(row[0])
-    f.close()
-    return post_ids
+def get_connection():
+    dsn = os.environ.get('DATABASE_URL')
+    return psycopg2.connect(dsn)
 
-def write_db(file_dir,new_ids):
-    db_path = f'{file_dir}/data/paper_database.csv'
-    with open(db_path,"a",newline='') as f: 
-        writer = csv.writer(f) 
-        for s_id in new_ids:
-            writer.writerow([s_id])
-    f.close()
+def read_db():
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+
+            post_short_ids = []
+            cur.execute('SELECT * FROM papers')
+            for row in cur:
+                post_short_ids.append(row[0])
+    return post_short_ids
 
 def check_db(file_dir,s_id):
-    post_ids = read_db(file_dir)
+    post_ids = read_db()
     
     if s_id in post_ids:
         return False
     else:
         return True
-        
+    
+def write_db(new_ids):
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            for new_id in new_ids:
+                cur.execute('INSERT INTO papers (short_id) VALUES (%s)', (new_id,))
+                
 import time
 import datetime
 from dateutil import tz
@@ -88,9 +89,9 @@ def get_paper():
         if check_db(file_dir, s_id):
             papers.append(paper)
             new_ids.append(s_id)
-    if len(new_ids) == 0:
-            new_ids.append("dammy")
-    write_db(file_dir,new_ids)
+            
+    if len(new_ids) != 0:
+        write_db(new_ids)
         
     return papers
 
